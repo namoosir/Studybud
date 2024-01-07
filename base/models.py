7cfd07773105a80223a194a98e63ea986ce7afa6
namoosir <saqeebnazmus@gmail.com>
2024-01-07 14:01:41 -0500

    update theme and make minor changes

--- base/models.py
+++ base/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Topic(models.Model):
     name = models.CharField(max_length=200)
 
@@ -15,7 +16,8 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -24,7 +26,7 @@ class Room(models.Model):
 
     def __str__(self):
         return str(self.name)
-    
+
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -33,6 +35,8 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return str(self.body)[:50]
-
