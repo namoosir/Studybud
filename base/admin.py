1313b63c988c3f765f3f8667ac24a629153cb337
namoosir <saqeebnazmus@gmail.com>
2024-01-08 05:28:46 -0500

    finishing touches

--- base/admin.py
+++ base/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import Room, Topic, Message
 
+class StudyBudAdminArea(admin.AdminSite):
+    index_title = "StudyBud"
+    site_header = "StudyBud Admin"
+    site_title = "StudyBud Admin"
+
+studybud_admin_site = StudyBudAdminArea()
+
+studybud_admin_site.register(Room)
+studybud_admin_site.register(Topic)
+studybud_admin_site.register(Message)
+
 admin.site.register(Room)
 admin.site.register(Topic)
 admin.site.register(Message)
--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 # Create your views here.
 
@@ -69,7 +69,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -172,3 +172,29 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': message})
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid:
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context= {'form': form}
+    return render(request, 'base/update-user.html', context)
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {'topics': topics}
+    return render(request, 'base/topics.html', context)
+
+def activitiesPage(request):
+    room_messages = Message.objects.all()
+    context = {'room_messages': room_messages}
+    return render(request, 'base/activity.html', context)
